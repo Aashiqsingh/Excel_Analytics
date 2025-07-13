4fc27e74951e61b44b1b23089f30c436f7738697
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-13 14:57:31 +0530

    ..

--- react_frontend/src/component/Login.jsx
+++ react_frontend/src/component/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(formData); // handle login logic here
-        const resopnse = await axios.post("https://excel-analytics-Node.onrender.com/user/login", formData);
+        const resopnse = await axios.post("https://excel-analytics-yl67.onrender.com/user/login", formData);
         console.log(resopnse);
         if (resopnse.status === 200) {
             toast.success('Login Successfully...', {
